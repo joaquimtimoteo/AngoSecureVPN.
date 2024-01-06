6d766dcd310af905b259499b86b8f12b5a01aa56
Joaquim Timóteo <109276363+joaquimtimoteo@users.noreply.github.com>
2024-01-06 00:25:35 +0000

    AngoSecureVPN

---
+++ lib/allModels/network_ip_info.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class NetworkIPInfo
+{
+  String titleText;
+  String subtitleText;
+  Icon iconData;
+
+  NetworkIPInfo({
+    required this.titleText,
+    required this.subtitleText,
+    required this.iconData,
+  });
+}
